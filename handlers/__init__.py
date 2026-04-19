d0f8cc3d5f1361408a9b3630ce37b21b712d6265
creator154 <cteator898@gmail.com>
2026-04-19 19:27:19 +0530

    Delete handlers directory

--- handlers/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import start
-from . import create
-from . import question
-from . import settings
-from . import launch
--- handlers/start.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-@Client.on_message(filters.command("start"))
-async def start(client, message):
-    buttons = [
-        [InlineKeyboardButton("➕ Create Quiz", callback_data="create")],
-    ]
-    await message.reply("Welcome to Quiz Bot", reply_markup=InlineKeyboardMarkup(buttons))
