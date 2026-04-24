1a5c98108d37e3a57e45e1e00e215bdd533fa810
creator154 <cteator898@gmail.com>
2026-04-25 02:09:43 +0530

    Refactor bot.py to use environment variable for token

--- bot.py
+++ bot.py
@@ -1,20 +1,18 @@
+import os
 from telegram.ext import Application
-from config import BOT_TOKEN
 
 from handlers.start import start_handler
 from handlers.create import create_handler
-from handlers.buttons import button_handler
-from handlers.ready import ready_handler
-from handlers.answer import answer_handler
+from handlers.quiz import quiz_handler
+
+TOKEN = os.getenv("BOT_TOKEN")
 
 def main():
-    app = Application.builder().token(BOT_TOKEN).build()
+    app = Application.builder().token(TOKEN).build()
 
     app.add_handler(start_handler)
     app.add_handler(create_handler)
-    app.add_handler(button_handler)
-    app.add_handler(ready_handler)
-    app.add_handler(answer_handler)
+    app.add_handler(quiz_handler)
 
     print("Bot running...")
     app.run_polling()
