48c6e0033542ec846a897a646b05cc736463d0d9
creator154 <cteator898@gmail.com>
2026-04-27 03:14:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,27 @@
-import os,asyncio
-from dotenv import load_dotenv
-from aiogram import Bot,Dispatcher
+import asyncio
+import os
+from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-from database import init_db
-
-from handlers.create_quiz import router as create_router
-from handlers.take_quiz import router as take_router
-from handlers.leaderboard import router as lb_router
-from handlers.edit_quiz import router as edit_router
+from dotenv import load_dotenv
 
 load_dotenv()
+BOT_TOKEN=os.getenv('BOT_TOKEN')
 
-bot=Bot(os.getenv("BOT_TOKEN"))
+if not BOT_TOKEN:
+    raise ValueError('BOT_TOKEN missing in .env')
+
+bot=Bot(token=BOT_TOKEN)
 dp=Dispatcher(storage=MemoryStorage())
 
+# IMPORT ONLY CURRENT ROUTERS
+from handlers.create_quiz import router as create_router
+
+# include ONLY once
 dp.include_router(create_router)
-dp.include_router(take_router)
-dp.include_router(lb_router)
-dp.include_router(edit_router)
 
 async def main():
-    await init_db()
+    print('Bot Started...')
     await dp.start_polling(bot)
 
-if __name__=="__main__":
+if __name__=='__main__':
     asyncio.run(main())
