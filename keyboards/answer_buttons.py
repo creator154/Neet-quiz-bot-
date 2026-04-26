a72b34d08f3de3dee6ba1f23b3c823ea186a2e9e
creator154 <cteator898@gmail.com>
2026-04-27 02:05:00 +0530

    Add answer keyboard with buttons A, B, C, D

--- keyboards/answer_buttons.py
+++ keyboards/answer_buttons.py
@@ -1,1 +1,11 @@
+from aiogram.types import InlineKeyboardMarkup,InlineKeyboardButton
 
+def answer_kb():
+    return InlineKeyboardMarkup(
+      inline_keyboard=[
+       [InlineKeyboardButton(text='A',callback_data='0')],
+       [InlineKeyboardButton(text='B',callback_data='1')],
+       [InlineKeyboardButton(text='C',callback_data='2')],
+       [InlineKeyboardButton(text='D',callback_data='3')],
+      ]
+    )
