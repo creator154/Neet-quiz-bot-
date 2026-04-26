a0e83e5a0ac82066d76b9307ebf1a448e9ed002f
creator154 <cteator898@gmail.com>
2026-04-27 02:03:42 +0530

    Update menu.py

--- keyboards/menu.py
+++ keyboards/menu.py
@@ -1,1 +1,18 @@
+from aiogram.types import InlineKeyboardMarkup,InlineKeyboardButton
 
+main_menu = InlineKeyboardMarkup(
+inline_keyboard=[
+[
+InlineKeyboardButton(
+text="Create Quiz",
+callback_data="create_quiz"
+)
+],
+[
+InlineKeyboardButton(
+text="Start Quiz",
+callback_data="start_quiz"
+)
+]
+]
+)
