15a7dc3a79acf188263e32a2cc8776f1a7812714
creator154 <cteator898@gmail.com>
2026-04-27 02:06:22 +0530

    Update quiz_states.py

--- states/quiz_states.py
+++ states/quiz_states.py
@@ -1,1 +1,12 @@
+from aiogram.fsm.state import State, StatesGroup
 
+class QuizCreation(StatesGroup):
+    title = State()
+    timer = State()
+    negative = State()
+    question = State()
+    options = State()
+    correct = State()
+
+class QuizPlay(StatesGroup):
+    answering = State()
