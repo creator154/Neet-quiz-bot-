cbf12fd4bad8ca9a06f84725993efe16ef88af49
creator154 <cteator898@gmail.com>
2026-04-27 02:04:25 +0530

    Add timer buttons to InlineKeyboardMarkup

--- keyboards/timer_buttons.py
+++ keyboards/timer_buttons.py
@@ -1,1 +1,19 @@
+from aiogram.types import InlineKeyboardMarkup,InlineKeyboardButton
 
+timer_keyboard=InlineKeyboardMarkup(
+inline_keyboard=[
+[
+InlineKeyboardButton(text="10",callback_data="10"),
+InlineKeyboardButton(text="15",callback_data="15"),
+InlineKeyboardButton(text="20",callback_data="20")
+],
+[
+InlineKeyboardButton(text="30",callback_data="30"),
+InlineKeyboardButton(text="45",callback_data="45"),
+InlineKeyboardButton(text="60",callback_data="60")
+],
+[
+InlineKeyboardButton(text="75",callback_data="75")
+]
+]
+)
