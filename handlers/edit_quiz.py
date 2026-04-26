7daf51586a2be2cbc8106d51e828fe3f410157f5
creator154 <cteator898@gmail.com>
2026-04-27 01:54:26 +0530

    Update edit_quiz.py

--- handlers/edit_quiz.py
+++ handlers/edit_quiz.py
@@ -1,1 +1,19 @@
+import aiosqlite
+from aiogram import Router
+from aiogram.filters import CommandObject,Command
+from aiogram.types import Message
 
+router=Router()
+
+@router.message(Command('edit_title'))
+async def edit(m:Message, command:CommandObject):
+ # /edit_title QUIZID|New title
+ if not command.args:
+   return
+ qid,title=command.args.split('|',1)
+ async with aiosqlite.connect('quiz.db') as db:
+   await db.execute(
+   'update quizzes set title=? where id=?',(title,qid)
+   )
+   await db.commit()
+ await m.answer('Quiz updated')
